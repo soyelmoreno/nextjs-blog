80cc4f9b743ed86fc27d912251ca2722099d89a9
Carlos Moreno <soyelmoreno@gmail.com>
2020-12-23 17:51:11 -0800

    - Install gray-matter - Add a couple of blog posts as markdown files - Create library `posts.js` for processing markdown files - Create async function getStaticProps() that grabs data from posts and sends it to Home() as props. - Tell Home() to display list of blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,47 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
+      {/* Picture and description */}
       <section className={utilStyles.headingMd}>
         <p>I like to run around learning things.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+
+      {/* List of posts */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
