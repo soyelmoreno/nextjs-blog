e5994a3f65c91838e13dd783a53ac5bf141ef570
Carlos Moreno <soyelmoreno@gmail.com>
2021-01-21 16:16:43 -0800

    - Create a dynamic route [id].js that renders a post dynamically. - Create function getAllPostIds() to return a list of file names in the posts directory. Use this inside getStaticPaths(). - Create function getPostData(id) that fetches the necessary data for rendering a post with the given id. Use this inside getStaticProps().

---
+++ pages/posts/[id].js
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, { siteTitle } from '../../components/Layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+/**
+ * Fetch necessary data (and return it as props) to render the post with the
+ * given id.
+ */
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData
+    }
+  };
+}
+
+/** Return the list of file names (excluding .md) in the posts directory */
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>
+          {postData.title} | {siteTitle}
+        </title>
+      </Head>
+      <h1>{postData.title}</h1>
+      <h2>
+        {postData.id} - {postData.date}
+      </h2>
+      <div>Post content goes here</div>
+    </Layout>
+  );
+}
