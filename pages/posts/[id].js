58db5dd4984ed708be9cecf11bedb8df727e6870
Carlos Moreno <soyelmoreno@gmail.com>
2021-01-21 17:34:09 -0800

    - Create Date component to format post date - Add some style to dynamic post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Date from '../../components/Date';
 import Layout, { siteTitle } from '../../components/Layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
 
 /**
  * Fetch necessary data (and return it as props) to render the post with the
@@ -33,10 +35,10 @@ export default function Post({ postData }) {
           {postData.title} | {siteTitle}
         </title>
       </Head>
-      <h1>{postData.title}</h1>
-      <h2>
-        {postData.id} - {postData.date}
-      </h2>
+      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+      <div className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+      </div>
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
